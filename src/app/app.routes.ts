0084469afff849bfbb7cabfb25da5446f08dd8d1
leonardopeixe <dev.leonardopeixe@outlook.com>
2024-05-19 17:54:13 -0300

    Correção de navbar para exibir rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
+import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
+
+// Rotas do app
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { EditComponent } from './edit/edit.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'edit/:id', component: EditComponent }
+	{ path: '', redirectTo: '/decks', pathMatch: 'full'},
+	{ path: 'decks', component: HomeComponent, data: { text: 'Visualizar Decks' }},
+  { path: 'about', component: AboutComponent, data: { text: 'Sobre o app' } },
+  { path: 'edit/:id', component: EditComponent},
+	{ path: 'error', component: UncaughtErrorComponent },
+	{ path: '**', component: PageNotFoundComponent } // must always be last
 ];
